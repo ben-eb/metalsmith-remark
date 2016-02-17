948778ea3913f3609f8e1549274f601c8fe07fd7
Ben Briggs <beneb.info@gmail.com>
2016-02-17 00:07:05 +0000

    Tidy up module.
    * Bump all dependencies (inc. upgrade to remark 4).
    * Now compiled with babel 6.
    * Use shared eslint config.
    
    Closes #4, closes #5, closes #6, closes #7, closes #8, closes #9
    and closes #10.

---
+++ src/__tests__/test.js
@@ -0,0 +1,20 @@
+import ava from 'ava';
+import equal from 'assert-dir-equal';
+import remark from '..';
+import rmBadges from 'remark-strip-badges';
+import rmParas from 'remark-squeeze-paragraphs';
+import metalsmith from 'metalsmith';
+
+ava('should convert markdown files', t => {
+    return new Promise(resolve => {
+        metalsmith('fixtures')
+            .use(remark([ rmBadges, rmParas ]))
+            .build(err => {
+                t.notOk(err, 'should not error');
+                t.doesNotThrow(() => {
+                    equal('fixtures/build', 'fixtures/expected');
+                });
+                resolve();
+            });
+    });
+});
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import remark from 'remark';
+import html from 'remark-html';
+import {extname} from 'path';
+
+export default function metalsmithRemark (plugins) {
+    return function (files) {
+        Object.keys(files).map(file => {
+            const extension = extname(file);
+            if (extension !== '.md' && extension !== '.markdown') {
+                return true;
+            }
+            const markdown = String(files[file].contents);
+            const result = remark.use(plugins).use(html).process(markdown);
+            files[file].contents = new Buffer(result);
+            const data = files[file];
+            delete files[file];
+            files[file.replace(extension, '.html')] = data;
+        });
+    };
+}
