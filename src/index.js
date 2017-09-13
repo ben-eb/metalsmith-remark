b9617842d66c885fc590a6fb80c8e363cce1545f
John Reinier Bulaong <rein.bulaong@yahoo.com>
2017-09-13 16:03:39 +0800

    Update for remark@8.0.0 and remark-html@6.0.1

--- src/index.js
+++ src/index.js
@@ -3,18 +3,19 @@ import remark from 'remark';
 import html from 'remark-html';
 
 export default function metalsmithRemark (plugins) {
-    return function (files) {
-        Object.keys(files).map(file => {
+    return function (files, metal, done) {
+        Promise.all(Object.keys(files).map(file => {
             const extension = extname(file);
             if (extension !== '.md' && extension !== '.markdown') {
                 return true;
             }
             const markdown = String(files[file].contents);
-            const result = remark().use(plugins).use(html).process(markdown);
-            files[file].contents = new Buffer(result.contents);
-            const data = files[file];
-            delete files[file];
-            files[file.replace(extension, '.html')] = data;
-        });
+            return remark().use(plugins).use(html).process(markdown).then(result => {
+                files[file].contents = new Buffer(result.contents);
+                const data = files[file];
+                delete files[file];
+                files[file.replace(extension, '.html')] = data;
+            });
+        })).then(() => done());
     };
 }
